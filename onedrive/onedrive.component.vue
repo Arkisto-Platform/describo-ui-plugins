7f9d43e2e0f6d0c7dd9ee144222f2d65a7ca6ece
Marco La Rosa <m@lr.id.au>
2020-10-16 14:55:17 +1100

    revise to emit account and token data; bugfix

--- onedrive/onedrive.component.vue
+++ onedrive/onedrive.component.vue
@@ -1,38 +1,34 @@
 <template>
-    <div class="flex flex-col">
-        <div v-if="!account && !loggingIn && !loggedIn">
-            <el-button type="primary" @click.prevent="login"
-                >login to Microsoft OneDrive</el-button
+    <div class="flex flex-row">
+        <div class="mr-2">
+            <el-button
+                type="primary"
+                @click.prevent="login"
+                :disabled="loggedIn || loggingIn"
             >
+                login to Microsoft OneDrive
+            </el-button>
         </div>
-        <div v-else>
-            <div v-if="loggingIn">
-                Please wait a moment whilst you're logged in...
-                <el-select
-                    v-if="drives && drives.length"
-                    v-model="selectedDrive"
-                    placeholder="Select the OneDrive to work with"
-                    @change="emitRcloneConfigurationData"
+        <div v-if="loggingIn">
+            <el-select
+                v-if="drives && drives.length"
+                class="w-full"
+                v-model="selectedDrive"
+                placeholder="Select the OneDrive to work with"
+                @change="emitRcloneConfigurationData"
+            >
+                <el-option
+                    v-for="drive of drives"
+                    :key="drive.id"
+                    :label="drive.id"
+                    :value="drive.id"
                 >
-                    <el-option
-                        v-for="drive in drives"
-                        :key="drive.id"
-                        :label="driveLabel(drive)"
-                        :value="drive.id"
-                    >
-                        <span style="float: left">{{ drive.driveType }}</span>
-                        <span
-                            style="
-                                float: right;
-                                color: #8492a6;
-                                font-size: 13px;
-                            "
-                            >{{ drive.id }}</span
-                        >
-                    </el-option>
-                </el-select>
-            </div>
-            <div v-if="loggedIn">You are logged in to your OneDrive!</div>
+                    <span class="text-left">{{ drive.driveType }}</span>
+                    <span class="text-right text-gray-600">
+                        {{ drive.id }}
+                    </span>
+                </el-option>
+            </el-select>
         </div>
     </div>
 </template>
@@ -113,11 +109,11 @@ export default {
                 if (drives.length === 1) this.selectedDrive = drives[0];
                 this.account = account;
                 this.token = token;
-                // let drive = await client.api("/me/drive").get();
-                // console.log(drive);
-                this.loggingIn = false;
-                this.loggedIn = true;
-                this.emitRcloneConfigurationData();
+                this.$emit("account", account);
+                this.$emit("token", token);
+                if (this.selectedDrive) {
+                    this.emitRcloneConfigurationData();
+                }
             }
         },
         emitRcloneConfigurationData() {
@@ -127,6 +123,8 @@ export default {
                 drive_id: this.selectedDrive.id,
                 drive_type: this.selectedDrive.driveType,
             });
+            this.loggingIn = false;
+            this.loggedIn = true;
         },
     },
 };

cc42a00c7343d837f911f4e8478c0b7a3c4c2d44
Marco La Rosa <m@lr.id.au>
2021-09-21 12:09:18 +1000

    implement preview component

--- s3/preview.component.vue
+++ s3/preview.component.vue
@@ -1,31 +1,38 @@
 <template>
-    <div class="flex flex-col text-center space-y-4">
-        <div>
-            Unfortunately file previews from owncloud are not possible at this time. However, here's
-            a link to the folder in owncloud that you can open in another window.
-        </div>
-        <div>
-            <a :href="link" target="_blank" class="text-yellow-600"
-                >open the owncloud folder in another window</a
-            >
-        </div>
+    <div>
+        <viewer-component :link="link" :entity="entity" />
     </div>
 </template>
 
 <script>
 export default {
+    props: {
+        path: {
+            type: String,
+        },
+        entity: {
+            type: Object,
+        },
+    },
     data() {
         return {
             link: undefined,
+            error: false,
         };
     },
     mounted() {
-        this.getOwncloudPath();
+        this.getPresignedUrl();
     },
     methods: {
-        async getOwncloudPath() {
-            let server = await this.owncloudAuthenticationManager.getServerInformation();
-            this.link = `${server.url}/apps/files/?dir=${this.$store.state.target.folder.path}`;
+        async getPresignedUrl() {
+            let response = await this.$http.post({
+                route: `/s3/presigned-url`,
+                body: { file: this.path },
+            });
+            if (response.status !== 200) {
+                this.error = true;
+            }
+            this.link = (await response.json()).url;
         },
     },
 };
